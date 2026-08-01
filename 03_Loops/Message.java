7e5d25804c7e609852f4fb6668f37d11a7d4b71d
JurassicParth <PES1UG25CA095@stu.pes.edu>
2026-08-01 19:10:33 +0530

    Add loop to display 'Happy New Year!!!'
    Fix loop initialization and output message.

---
+++ 03_Loops/Message.java
@@ -0,0 +1,9 @@
+// To display a message on the screen
+public class Message {
+  public static void main(String[] args){
+    int a;
+    for(a = i; a <= 10; a++){
+      System.out.println("Happy New Year!!!");
+    }
+  }
+}
