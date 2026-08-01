6e5aa8e8d2d84f317f95210a8dbf44202e214c95
JurassicParth <PES1UG25CA095@stu.pes.edu>
2026-08-01 18:24:47 +0530

    Add Rectangle area and perimeter calculation
    Implemented a Java program to calculate the area and perimeter of a rectangle based on user input.

---
+++ 02_Conditional_Statements/Rectangle.java
@@ -0,0 +1,18 @@
+// To find the area and perimeter of a rectangle
+import java.util.*;
+public class Rectangle {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int l, b, p, ar;
+    System.out.println("Enter length: ");
+    l = sc.nextInt();
+    System.out.println("Enter breadth: ");
+    b = sc.nextInt();
+    
+    ar = l * b;
+    p = 2 * (l + b);
+
+    System.out.println("Area = " + ar);
+    System.out.println("Perimeter = " + p);
+  }
+}
