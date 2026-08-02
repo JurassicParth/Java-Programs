909cc0b18a03b8adc94e30e360e4b0aafa32762c
JurassicParth <PES1UG25CA095@stu.pes.edu>
2026-08-02 13:03:13 +0530

    Implement StringArray class to print names

---
+++ 04_Arrays/StringArray.java
@@ -0,0 +1,10 @@
+public class StringArray {
+  public static void main(String args[]) {
+    int i;
+    String m[] = {"Amit", "sumit", "Rohit", "Vijay", "Mohit"};
+    System.out.println("The names are:-");
+    for(i = 0; i < 5; i++) {
+      System.out.println(m[i]);
+    }
+  }
+}
