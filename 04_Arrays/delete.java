c6d7b87ca3625ba9d4492decaa8eedfc296b7414
JurassicParth <PES1UG25CA095@stu.pes.edu>
2026-08-02 13:48:29 +0530

    Add delete.java to remove an element from an array
    Implement a Java program to delete an element from an array, including user input for array size and position of deletion.

---
+++ 04_Arrays/delete.java
@@ -0,0 +1,27 @@
+// A sample program to delete an element from a set array elements
+import java.util.*;
+class delete {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int i;
+    int n;
+    iny pos;
+    int arr[] = new int[50];
+    System.out.println("Enter number of array elements: ");
+    n = sc.nextInt();
+    System.out.println("Enter array elements: ");
+    for(i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("Enter position of element to be deleted: ");
+    pos = sc.nextInt();
+    for(i = pos + 1; i < n; i++) {
+      arr[i - 1] = arr[i];
+    }
+    n--;
+    System.out.println("Array elements after deletion:-");
+    for(i = 0; i < n; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
